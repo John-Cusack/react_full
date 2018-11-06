29d04c75c6d1c45f89c95b427fca1b8267b90857
john <johncusackccm@gmail.com>
2018-11-05 22:24:22 -0500

    tried to fix

--- server/index.js
+++ server/index.js
@@ -1,14 +1,16 @@
 const express = require('express');
-const mongoose = require('mongoose')
-const keys = require('./config/keys')
-require('./models/User')
-require('./services/passport')
+const mongoose = require('mongoose');
+const passport = require('passport');
+const keys = require('./config/keys');
+require('./models/User');
+require('./services/passport');
 
-mongoose.connect(keys.mongoURI)
+mongoose.connect(keys.mongoURI, {
+    useNewUrlParser: true
+});
 
 const app = express();
+require('./routes/authRoutes')(app);
 
-require('./routes/authRoutes')(app)
-
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 app.listen(PORT);
--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,10 +1,13 @@
 const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 module.exports = app => {
-    app.get('/auth/google', passport.authenticate('google', {
-        scope: ['profile', 'email']
-    }))
 
-    app.get('/auth/google/callback', passport.authenticate('google'))
-}
+    app.get(
+        '/auth/google',
+        passport.authenticate('google', {
+            scope: ['profile', 'email']
+        })
+    );
+
+    app.get('/auth/google/callback', passport.authenticate('google'));
+};
